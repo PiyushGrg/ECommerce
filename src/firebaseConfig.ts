0bc9c3a6c15660a9dd6eca581a7f9e033c08c6f8
Piyush Garg <123235977+PiyushGrg@users.noreply.github.com>
2024-05-15 22:21:59 +0530

    Storage Bucket fix

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
   projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
+  storageBucket: "gs://ecommerce-5071c.appspot.com",
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
   appId: process.env.APP_ID,
   measurementId: process.env.MEASUREMENT_ID
